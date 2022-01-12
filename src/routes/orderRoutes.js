e457fc4ea40ff23f6f8f4d06713923f038160e55
UWAJENEZANadine <adelineu640@gmail.com>
2022-01-12 10:25:26 +0200

    verify access additional

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import orderController from "../controllers/orderController";
+import verfyAccess from "../middlewares/verifyAccess";
+import verifyToken from "../middlewares/verifyToken"
+ 
 
 const orderRouter = express.Router(); 
 orderRouter.post("/create", orderController.createOrder)
 orderRouter.get("/all", orderController.getallOrder)
 orderRouter.get("/:id",orderController.getOneOrder)
 orderRouter.delete("/:id",orderController.deleteOneOrder)
-orderRouter.patch("/all", orderController.patchallOrder)
+orderRouter.patch("/all", verifyToken, verfyAccess("admin"), orderController.patchallOrder)
 
 export default orderRouter;
